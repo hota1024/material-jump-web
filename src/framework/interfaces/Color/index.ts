05dc5f9956039fa7c08d95c9e9fd924487d04029
hota1024 <hotalog@hota1024.com>
2020-01-01 23:47:25 +0900

    Add ColorInterface

---
+++ src/framework/interfaces/Color/index.ts
@@ -0,0 +1,24 @@
+/**
+ * Color interface.
+ */
+export interface ColorInterface {
+  /**
+   * Red.
+   */
+  r: number
+
+  /**
+   * Green.
+   */
+  g: number
+
+  /**
+   * Blue.
+   */
+  b: number
+
+  /**
+   * To hex code.
+   */
+  toHexCode(): string
+}
