1585c97df94ebf9a70d61dedccb23fed11331648
hota1024 <hotalog@hota1024.com>
2020-01-03 16:27:50 +0900

    :label: Add FontOptions type

---
+++ src/framework/types/FontOptions/index.ts
@@ -0,0 +1,21 @@
+import { TextAlign } from '../TextAlign'
+
+/*
+ * Font style.
+ */
+export type FontOptions = {
+  /**
+   * Size.
+   */
+  size?: string
+
+  /**
+   * Font family.
+   */
+  family?: string
+
+  /**
+   * Text align.
+   */
+  align?: TextAlign
+}
