d5c10ab6442db0822721ae073cd79b95fed02106
hota1024 <hotalog@hota1024.com>
2020-01-03 16:26:04 +0900

    :label: Add TextAlign enum type

---
+++ src/framework/types/TextAlign/index.ts
@@ -0,0 +1,29 @@
+/*
+ * Text align.
+ */
+export enum TextAlign {
+  /**
+   * Start.
+   */
+  Start,
+
+  /**
+   * End.
+   */
+  End,
+
+  /**
+   * Left.
+   */
+  Left,
+
+  /**
+   * Right.
+   */
+  Right,
+
+  /**
+   * Center.
+   */
+  Center
+}
