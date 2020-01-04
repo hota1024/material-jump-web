cb0faadbd58d9cc8233d9ec8ddbc1f28eadfcab0
hota1024 <hotalog@hota1024.com>
2020-01-04 21:02:04 +0900

    :label: Add CanvasRendererInvalidContextError

---
+++ src/framework/impls/renderers/CanvasRenderer/errors/CanvasRendererInvalidContext/index.ts
@@ -0,0 +1,11 @@
+import { FrameworkError } from '@/src/framework/classes/FrameworkError'
+
+/**
+ * CanvasRenderingInvalidContext error.
+ */
+export class CanvasRendererInvalidContextError extends FrameworkError {
+  /**
+   * Name.
+   */
+  name = 'CanvasRendererInvalidContextError'
+}
